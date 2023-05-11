aa5ebe3c6f2caebece47275830acfac89b78e8d7
clwater <gzb@clwater.top>
2023-05-11 15:15:54 +0800

    launchedEffect adn rememberCoroutineScope

--- app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
+++ app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.clwater.compose_learn_1.ui.theme.Compose_Learn_1Theme
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
@@ -26,7 +25,8 @@ class MainActivity : ComponentActivity() {
     class LaunchedEffectTestViewModel : ViewModel() {
 //        val showSnackbar = mutableStateOf(false)
         val snackbarCount = mutableStateOf(0)
-        val showScaffold = mutableStateOf(true)
+        val showInnerButton = mutableStateOf(true)
+        val showCommonButton = mutableStateOf(true)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,10 +53,48 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Test1(viewModel: LaunchedEffectTestViewModel) {
-        if (viewModel.showScaffold.value) {
-            LaunchedEffectTestScreen(
-                snackbarHostState = SnackbarHostState(),
-                viewModel = viewModel
+//        if (viewModel.showScaffold.value) {
+        LaunchedEffectTestScreen(
+            snackbarHostState = SnackbarHostState(),
+            viewModel = viewModel
+        )
+//        }
+    }
+
+    @Composable
+    fun ScopeButton(
+        snackbarHostState: SnackbarHostState,
+        viewModel: LaunchedEffectTestViewModel
+    ) {
+        val scope = rememberCoroutineScope()
+
+        Button(
+            onClick = {
+                scope.launch {
+                    Log.d(
+                        "clwater",
+                        "(Inner Coroutine Scope): displaying " +
+                            "launched effect for count " +
+                            "${viewModel.snackbarCount.value}"
+                    )
+                    try {
+                        snackbarHostState.showSnackbar(
+                            "(Inner Coroutine Scope): LaunchedEffect snackbar: " +
+                                " ${viewModel.snackbarCount.value}"
+                        )
+                    } catch (e: Exception) {
+                        Log.e(
+                            "clwater",
+                            "(Inner Coroutine Scope): launched Effect coroutine " +
+                                "cancelled exception $e"
+                        )
+                    }
+                }
+            }
+        ) {
+            Text(
+                text = "(Inner Coroutine Scope): Show current snackbar, " +
+                    "count: ${viewModel.snackbarCount.value}"
             )
         }
     }
@@ -72,23 +110,18 @@ class MainActivity : ComponentActivity() {
         LaunchedEffect(viewModel.snackbarCount.value) {
             Log.d(
                 "clwater",
-                "LaunchedEffect: displaying launched effect for count " +
+                "(LaunchedEffect): displaying launched effect for count " +
                     "${viewModel.snackbarCount.value}"
             )
             try {
                 snackbarHostState.showSnackbar(
-                    "LaunchedEffect: LaunchedEffect snackbar: " +
+                    "(LaunchedEffect): LaunchedEffect snackbar: " +
                         " ${viewModel.snackbarCount.value}"
                 )
-                delay(1000)
-                Log.d(
-                    "clwater",
-                    "delay finish for count ${viewModel.snackbarCount.value}"
-                )
             } catch (e: Exception) {
-                Log.d(
+                Log.e(
                     "clwater",
-                    "LaunchedEffect: launched Effect coroutine cancelled exception $e"
+                    "(LaunchedEffect): launched Effect coroutine cancelled exception $e"
                 )
             }
         }
@@ -99,16 +132,20 @@ class MainActivity : ComponentActivity() {
             Column(Modifier.padding(12.dp)) {
                 Text(text = "Test LaunchedEffect")
                 Text(text = "Snackbar count: ${viewModel.snackbarCount.value}")
+
+                Spacer(modifier = Modifier.height(12.dp))
                 Button(
                     onClick = {
                         viewModel.snackbarCount.value += 1
                     }
                 ) {
                     Text(
-                        text = "(without scope): Add current snackbar, count: " +
+                        text = "(Without Scope): Add current snackbar, count: " +
                             "${viewModel.snackbarCount.value}"
                     )
                 }
+
+                Spacer(modifier = Modifier.height(12.dp))
 
                 Button(
                     onClick = {
@@ -116,39 +153,72 @@ class MainActivity : ComponentActivity() {
                     }
                 ) {
                     Text(
-                        text = "(in scope): Add current snackbar, count: " +
+                        text = "(In Scope): Add current snackbar, count: " +
                             "${viewModel.snackbarCount.value}"
                     )
                 }
 
-                Button(
-                    onClick = {
-                        scope.launch {
-                            snackbarHostState.showSnackbar(
-                                "(snackbarCount change): LaunchedEffect snackbar: " +
-                                    " ${viewModel.snackbarCount.value}"
-                            )
-                            delay(1000)
-                            Log.d(
-                                "clwater",
-                                "(snackbarCount change): delay finish for count ${viewModel.snackbarCount.value}"
-                            )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                if (viewModel.showCommonButton.value) {
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                Log.d(
+                                    "clwater",
+                                    "(In Common Coroutine Scope): displaying " +
+                                        "launched effect for count " +
+                                        "${viewModel.snackbarCount.value}"
+                                )
+                                try {
+                                    snackbarHostState.showSnackbar(
+                                        "(In Common Coroutine Scope): LaunchedEffect snackbar: " +
+                                            " ${viewModel.snackbarCount.value}"
+                                    )
+                                } catch (e: Exception) {
+                                    Log.d(
+                                        "clwater",
+                                        "(In Common Coroutine Scope): launched Effect coroutine " +
+                                            "cancelled exception $e"
+                                    )
+                                }
+                            }
                         }
+                    ) {
+                        Text(
+                            text = "(In Common Coroutine Scope): Show current snackbar, " +
+                                "count: ${viewModel.snackbarCount.value}"
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                if (viewModel.showInnerButton.value) {
+                    ScopeButton(
+                        snackbarHostState = snackbarHostState,
+                        viewModel = viewModel
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+
+                Button(
+                    onClick = {
+                        viewModel.showCommonButton.value = false
                     }
                 ) {
                     Text(
-                        text = "(in scope): Show current snackbar, " +
-                            "count: ${viewModel.snackbarCount.value}"
+                        text = "hide showCommonButton"
                     )
                 }
 
                 Button(
                     onClick = {
-                        viewModel.showScaffold.value = false
+                        viewModel.showInnerButton.value = false
                     }
                 ) {
                     Text(
-                        text = "hide Scaffold"
+                        text = "hide showInnerButton"
                     )
                 }
             }
