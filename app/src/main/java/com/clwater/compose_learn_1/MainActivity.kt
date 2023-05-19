82917c90820b9ee3739ae1516ef46449e9bdba09
clwater <gzb@clwater.top>
2023-05-19 16:48:33 +0800

    wip

--- app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
+++ app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.clwater.compose_learn_1.ui.theme.Compose_Learn_1Theme
-import java.lang.Exception
 import kotlinx.coroutines.delay
 
 /**
@@ -45,6 +43,8 @@ class MainActivity : ComponentActivity() {
         Log.d("clwater", "suspendFunTest Finish")
     }
 
+    // #1.1
+    // https://gist.github.com/clwater/5454deed3ae258ba3980d260a0ff3299
 //    @Composable
 //    fun TestLifecycleCompose() {
 //        LaunchedEffect(Unit) {
@@ -60,6 +60,7 @@ class MainActivity : ComponentActivity() {
 //    }
 
     // #1.2
+    // https://gist.github.com/clwater/4b9b1bedca4365732de7d8e2c8519190
 //    @Composable
 //    fun TestLifecycleCompose() {
 //        LaunchedEffect(Unit) {
@@ -69,36 +70,56 @@ class MainActivity : ComponentActivity() {
 //    }
 
     // #1.3
+    // https://gist.github.com/clwater/440af58717e699d4963f915f520d494a
+//    @Composable
+//    fun TestLifecycleCompose() {
+//        var isShow by remember {
+//            mutableStateOf(true)
+//        }
+//
+//        Column {
+//            if (isShow) {
+//                TestLifecycleComposeText()
+//            }
+//
+//            Button(
+//                onClick = { isShow = !isShow }
+//            ) {
+//                Text(text = "TestLifecycleCompose show: $isShow")
+//            }
+//        }
+//    }
+//
+//    @Composable
+//    fun TestLifecycleComposeText() {
+//        LaunchedEffect(Unit) {
+//            Log.d("clwater", "TestLifecycleCompose Enter")
+//            try {
+//                delay(10 * 1000)
+//                Log.d("clwater", "TestLifecycleCompose Finish")
+//            } catch (e: Exception) {
+//                Log.d("clwater", "TestLifecycleCompose Error: $e")
+//            }
+//        }
+//        Text(text = "TestLifecycleCompose")
+//    }
+
+    // # 1.4
+    // https://gist.github.com/clwater/e38e6e686a52652eda7c0ed23c7d9e0c
     @Composable
     fun TestLifecycleCompose() {
-        var isShow by remember {
-            mutableStateOf(true)
+        var clickCount by remember {
+            mutableStateOf(0)
+        }
+
+        LaunchedEffect(clickCount) {
+            Log.d("clwater", "TestLifecycleCompose clickCount: $clickCount")
         }
 
         Column {
-            if (isShow) {
-                TestLifecycleComposeText()
-            }
-
-            Button(
-                onClick = { isShow = !isShow }
-            ) {
-                Text(text = "TestLifecycleCompose show: $isShow")
+            Button(onClick = { clickCount++ }) {
+                Text("clickCount $clickCount")
             }
         }
-    }
-
-    @Composable
-    fun TestLifecycleComposeText() {
-        LaunchedEffect(Unit) {
-            Log.d("clwater", "TestLifecycleCompose Enter")
-            try {
-                delay(10 * 1000)
-                Log.d("clwater", "TestLifecycleCompose Finish")
-            } catch (e: Exception) {
-                Log.d("clwater", "TestLifecycleCompose Error: $e")
-            }
-        }
-        Text(text = "TestLifecycleCompose")
     }
 }
