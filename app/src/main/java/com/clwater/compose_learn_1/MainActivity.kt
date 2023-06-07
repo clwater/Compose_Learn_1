394e5ec64fd729fd4002a1904ec39468ceceda92
clwater <gzb@clwater.top>
2023-06-07 15:50:26 +0800

    wiP

--- app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
+++ app/src/main/java/com/clwater/compose_learn_1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.clwater.compose_learn_1
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -11,7 +10,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.clwater.compose_learn_1.ui.theme.Compose_Learn_1Theme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  * https://developer.android.com/jetpack/compose/side-effects?hl=zh-cn#state-effect-use-cases
@@ -172,70 +170,194 @@ class MainActivity : ComponentActivity() {
 //    }
 
     // #2.2
-    //https://gist.github.com/clwater/3195062cb4b74618bc06699325af62b5
+    // https://gist.github.com/clwater/3195062cb4b74618bc06699325af62b5
+//    @Composable
+//    fun TestLifecycleCompose() {
+//        var showChild by remember {
+//            mutableStateOf(true)
+//        }
+//
+//        var showParent by remember {
+//            mutableStateOf(true)
+//        }
+//
+//        val scope = rememberCoroutineScope()
+//
+//        Column {
+//            Row {
+//                Button(onClick = {
+//                    showParent = false
+//                }) {
+//                    Text(text = "Hide Parent")
+//                }
+//
+//                Button(onClick = {
+//                    showChild = false
+//                }) {
+//                    Text(text = "Hide Child")
+//                }
+//            }
+//
+//            if (showParent) {
+//                Button(onClick = {
+//                    scope.launch {
+//                        try {
+//                            Log.d("clwater", "TestLifecycleCompose")
+//                            delay(1000 * 1000)
+//                        } catch (e: Exception) {
+//                            Log.d("clwater", "TestLifecycleCompose Error: $e")
+//                        }
+//                    }
+//                }) {
+//                    Text(text = "Parent")
+//                }
+//            }
+//
+//            if (showChild) {
+//                TestLifecycleComposeChild()
+//            }
+//        }
+//    }
+//
+//    @Composable
+//    fun TestLifecycleComposeChild() {
+//        val scope = rememberCoroutineScope()
+//
+//        Button(onClick = {
+//            scope.launch {
+//                try {
+//                    Log.d("clwater", "TestLifecycleComposeChild")
+//                    delay(1000 * 1000)
+//                } catch (e: Exception) {
+//                    Log.d("clwater", "TestLifecycleComposeChild Error: $e")
+//                }
+//            }
+//        }) {
+//            Text(text = "Child")
+//        }
+//    }
+
+    // #3.1
+//    @Composable
+//    fun DelayCompose(click: Int) {
+//        val rememberClick = rememberUpdatedState(newValue = click)
+//        LaunchedEffect(Unit) {
+//            Log.d("clwater", "TestLifecycleCompose LaunchedEffect")
+//            delay(5000)
+//            Log.d("clwater", "TestLifecycleCompose click: $click")
+//            Log.d("clwater", "TestLifecycleCompose rememberClick: ${rememberClick.value}")
+//        }
+//    }
+//
+//    @Composable
+//    fun TestLifecycleCompose() {
+//        var lastClick by remember {
+//            mutableStateOf(-1)
+//        }
+//
+//        Column {
+//            Button(onClick = {
+//                Log.d("clwater", "onClick 0")
+//                lastClick = 0
+//            }) {
+//                Text(text = "0")
+//            }
+//
+//            Button(onClick = {
+//                Log.d("clwater", "onClick 1")
+//                lastClick = 1
+//            }) {
+//                Text(text = "1")
+//            }
+//        }
+//
+//        DelayCompose(click = lastClick)
+//    }
+    // #4.1
+    // https://gist.github.com/clwater/42f0ce7a5696d841664b7db5e868d33a
+//    @Composable
+//    fun TestLifecycleCompose(obsever: TestObserver) {
+//        LaunchedEffect(Unit){
+//            obsever.start()
+//        }
+//        DisposableEffect(Unit) {
+//            onDispose {
+//                obsever.stop()
+//            }
+//        }
+//    }
+
+    // #4.2
+    // https://gist.github.com/clwater/26c040dcf4680f0a2e0b48bf3cf304bb
+//    @Composable
+//    fun ControlCompose() {
+//        LaunchedEffect(Unit) {
+//            Log.d("clwater", "LaunchedEffect(Unit)")
+//        }
+//        DisposableEffect(Unit) {
+//            Log.d("clwater", "DisposableEffect(Unit) out onDispose")
+//            onDispose {
+//                Log.d("clwater", "DisposableEffect(Unit) in  onDispose")
+//            }
+//        }
+//
+//        var count by remember {
+//            mutableStateOf(0)
+//        }
+//
+//        Button(onClick = { count++ }) {
+//            Text(text = "count $count")
+//        }
+//
+//        LaunchedEffect(count) {
+//            Log.d("clwater", "LaunchedEffect(count)")
+//        }
+//        DisposableEffect(count) {
+//            Log.d("clwater", "DisposableEffect(count) out onDispose")
+//            onDispose {
+//                Log.d("clwater", "DisposableEffect(count) in  onDispose")
+//            }
+//        }
+//    }
+//
+//    @Composable
+//    fun TestLifecycleCompose() {
+//        var show by remember {
+//            mutableStateOf(true)
+//        }
+//
+//        Column {
+//            Button(onClick = {
+//                show = false
+//            }) {
+//                Text(text = "Hide")
+//            }
+//
+//            if (show) {
+//                ControlCompose()
+//            }
+//        }
+//    }
+
+    // #5.1.1
+    // https://gist.github.com/clwater/8b6bd66d1c60d4980d9a629818c4da40
     @Composable
     fun TestLifecycleCompose() {
-        var showChild by remember {
-            mutableStateOf(true)
-        }
-
-        var showParent by remember {
-            mutableStateOf(true)
-        }
-
-        val scope = rememberCoroutineScope()
-
-        Column {
-            Row {
-                Button(onClick = {
-                    showParent = false
-                }) {
-                    Text(text = "Hide Parent")
-                }
-
-                Button(onClick = {
-                    showChild = false
-                }) {
-                    Text(text = "Hide Child")
-                }
-            }
-
-            if (showParent) {
-                Button(onClick = {
-                    scope.launch {
-                        try {
-                            Log.d("clwater", "TestLifecycleCompose")
-                            delay(1000 * 1000)
-                        } catch (e: Exception) {
-                            Log.d("clwater", "TestLifecycleCompose Error: $e")
-                        }
-                    }
-                }) {
-                    Text(text = "Parent")
-                }
-            }
-
-            if (showChild) {
-                TestLifecycleComposeChild()
-            }
-        }
+        var text by remember { mutableStateOf("Common") }
+        Text(text = "text $text")
+        Thread.sleep(3 * 1000)
+        text = "Delay text"
     }
 
+    // #5.1.2
+    // https://gist.github.com/clwater/d6becf6352a6d915a08cc873b0d191ad
     @Composable
-    fun TestLifecycleComposeChild() {
-        val scope = rememberCoroutineScope()
-
-        Button(onClick = {
-            scope.launch {
-                try {
-                    Log.d("clwater", "TestLifecycleComposeChild")
-                    delay(1000 * 1000)
-                } catch (e: Exception) {
-                    Log.d("clwater", "TestLifecycleComposeChild Error: $e")
-                }
-            }
-        }) {
-            Text(text = "Child")
+    fun TestLifecycleCompose() {
+        var text by remember { mutableStateOf("Common") }
+        Text(text = "text $text")
+        SideEffect {
+            Thread.sleep(3 * 1000)
+            text = "Delay text"
         }
     }
 }
